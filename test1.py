5c7269442d4c6b46ba4135114f9c3ad85e63367a
MariaBrenzi <135764467+MariaBrenzi@users.noreply.github.com>
2024-03-13 14:30:17 +0100

    Initial commit

---
+++ test1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 16 10:19:43 2023
+
+@author: famil
+"""
+
+import pandas as pd
+import streamlit as st
+df=pd.read_csv('Kickstarter Campaigns DataSet.csv')
+st.write("Aperçu des données")
+st.dataframe(df.head())
+dfclean = pd.read_csv('df_Cleaning.csv')
+st.write("Aperçu des données clean")
+st.dataframe(dfclean.head())
